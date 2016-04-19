33568c56ef66e4bea64cf474fb2d448f04f66f99
fReDDy <zorge94@gmail.com>
2016-04-19 21:59:19 +0300

    Полностью реализована валидация соответствующих типов. Остается только игнорирование сохранения данных в случае если хотя бы какое то из условий ввода нарушено, а также добавить описание ошибки.

--- js/client.js
+++ js/client.js
@@ -2,6 +2,9 @@
  * Created by fReDDy on 17.04.2016.
  */
 document.addEventListener("DOMContentLoaded", function(){
+    $(document).ready(function(){
+        $(".errorClass").hide();
+    });
     $("#addString").click(function() {
         var tr = '<tr>';
         for (var i = 0; i < 3; i++)
@@ -11,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function(){
             '<option>System.Int32</option>' +
             '<option>System.Boolean</option>' +
             '</select></td>' +
-            '<td><input style="width:100%" type="text"></td>' +
+            '<td><input style="width:100%" type="text" name="text"></td>' +
             '<td><button style="width:100%" class="remove_row">delete</button></td>' +
             '</tr>';
         var trObject = $(tr);
@@ -21,6 +24,86 @@ document.addEventListener("DOMContentLoaded", function(){
     $(".remove_row").click(function(){
         removeRow();
     });
+    $("input[type='checkbox']").click(function(){
+        var trueCount=0;
+        $("input[type='checkbox']").each(function(){
+            if (this.checked)
+            {
+                trueCount++;
+            }
+        });
+        if (trueCount == 0)
+        {
+
+            $(".errorClass").show("slow");
+            $("input[type='checkbox']").each(function(){
+                this.style.borderColor == 'red';
+            });
+        }
+        else {
+            $(".errorClass").hide("slow");
+            $("input[type='checkbox']").each(function(){
+
+                //console.dir(this.style);
+            });
+        }
+    });
+    $("input").keyup(function() {
+        if (this.name == "text")
+        {
+            if (this.value.length >10)
+            {
+                this.style.borderColor='red';
+
+                $(".errorClass").show("slow");
+                console.dir(this.style);
+            }
+            else
+            {
+                this.style.borderColor='green';
+                $(".errorClass").hide("slow");
+            }
+        }
+        if (this.name == "number") {
+            checkIntForValidValue(this);
+        };
+        //console.log(this.value[0]);
+        //console.log(this.value[1]);
+    });
+    function checkIntForValidValue(currentEvent)
+    {
+        console.log(+currentEvent.value);
+        if (currentEvent.value[0] == "-" && currentEvent.value[1] == "0") {
+            currentEvent.style.borderColor='red';
+            console.log(+currentEvent.value);
+            $(".errorClass").show("slow");
+            return false;
+        }
+        //console.log("стала длина: "+this.value.length);
+        if (+currentEvent.value <= 256 && +currentEvent.value >= -256 &&
+            currentEvent.value.length !=0 && !(+currentEvent.value[0] == 0 && currentEvent.value.length > 1))
+        {
+            console.log("---"+currentEvent.value[0]);
+            currentEvent.style.borderColor='green';
+            //console.dir(currentEvent);
+            console.log(+currentEvent.value);
+
+            $(".errorClass").hide("slow");
+        }
+        else if (currentEvent.value.length == 0)
+        {
+
+            $(".errorClass").show("slow");
+            currentEvent.style.borderColor='red'
+            console.log("значение не может быть пустым!");
+        }
+        else
+        {
+
+            $(".errorClass").show("slow");
+            currentEvent.style.borderColor='red';
+        }
+    }
     $("#saveData").click(function(){
         console.log("--------------------------------------------");
        var result=[],
@@ -62,7 +145,14 @@ document.addEventListener("DOMContentLoaded", function(){
         else
         {
             var currentInput=$(this).parent().next()[0].children[0];
-            currentInput.type='text';
+            console.dir(this);
+            if (this.selectedIndex == '0') {
+                currentInput.name = 'text';
+            }
+            else if (this.selectedIndex == '1'){
+                currentInput.name = 'number';
+            }
+            currentInput.type = 'text';
             currentInput.value="";
         }
     });
